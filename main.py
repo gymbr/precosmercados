20304899b7a363065fd02f9da89b67b652a9c4a8
gymbr <dex.teste1@proton.me>
2025-10-29 21:20:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -299,7 +299,7 @@ def buscar_nagumo(term="banana"):
                 "storeReference": "22",
                 "currentPage": 1,
                 "minScore": 1,
-                "pageSize": 100,
+                "pageSize": 300,
                 "search": [{"query": term}],
                 "filters": {},
                 "googleAnalyticsSessionId": ""
